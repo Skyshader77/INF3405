241a0855f0c696522d2a0af3da8a3a2ac6e5e6d4
Alexandre Nguyen <71104409+Skyshader77@users.noreply.github.com>
2023-05-21 10:13:06 -0400

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -16,16 +16,16 @@ import java.net.ServerSocket;
 
 public class ClientHandler extends Thread{
 	private Socket socket;
+	private Server server;
 	private int clientNumber;
 	private PrintWriter out;
 	private String username;
 	private static final int AUTHENTICATED= 3;
 	public ClientHandler(Socket socket, int clientNumber, Server server) {
-		this.socket = socket; this.clientNumber = clientNumber;
+		this.socket = socket; this.clientNumber = clientNumber;this.server=server;
 		System.out.println("New connection with client#" + clientNumber + " at" + socket);
 		}
 	public void run() {   
-		// Création de thread qui envoi un message à un client 
 		try {
 			out = new PrintWriter(socket.getOutputStream(), true);
 			PassWordAuthentificationProtocol Pap=new PassWordAuthentificationProtocol();
@@ -34,30 +34,37 @@ public class ClientHandler extends Thread{
 			 // Initiate conversation with client
 		    outputLine = Pap.processInput(null);
 		    out.println(outputLine);
+		    //while the user has not been authenticated
 		    while (Pap.getState()<AUTHENTICATED) {
 		    	inputLine = bufferedIn.readLine();
 		        outputLine = Pap.processInput(inputLine);
 		        out.println(outputLine);
 		    }
 		    username=Pap.getUser();
+		    //while the user has not been disconnected
 		    while ((inputLine = bufferedIn.readLine()) != null) {
-		        out.println(username+inputLine);
-		        if (inputLine.equalsIgnoreCase("bye"))
+		    	String formattedOutput=username+inputLine;
+		        out.println(formattedOutput);
+		        server.communicateBetweenClients(formattedOutput, this);
+		        if (inputLine.equalsIgnoreCase("disconnect"))
 		            break;
 		    }
-	        //String clientLeft = clientNumber + " has been swatted and was forced to disconnect.";
-	        //server.communicateBetweenClients(clientLeft, this);
 		} catch (IOException e) {
-			System.out.println("Error handling client# " + clientNumber + ": " + e);
+			System.out.println("Error handling client# " + clientNumber + ": " + e.getMessage());
+			e.printStackTrace();
 		} finally {
-		try {
-			//server.EliminateClient(clientNumber, this);
-			socket.close();
-		} catch (IOException e) {
-			System.out.println("Couldn't close a socket, what's going on?");
-		}
-			System.out.println("Connection with client# " + clientNumber + " closed");
-		}
+			try {
+				//deletes the user from the server
+			    server.EliminateClient(clientNumber, this);
+		        String clientLeft =username + " has been swatted and was forced to disconnect.";
+		        server.communicateBetweenClients(clientLeft, this);
+		        socket.close();
+			} catch (IOException e) {
+				System.out.println("Couldn't close a socket, what's going on?" + e.getMessage());
+	            e.printStackTrace();
+			}
+				System.out.println("Connection with " + username + " closed");
+			}
 		}
 	/**
 	 * @summary returns the printwriter of the clientHandler class
@@ -67,13 +74,5 @@ public class ClientHandler extends Thread{
 	public PrintWriter getWriter() {
 	        return this.out;
 	}
-	/**
-	 * @summary returns the PassWordAuthentificationProtocol of the clientHandler class
-	 * @author Alexandre Nguyen & Louis-Phlippe
-	 * @version 1.0 Last modified on 19/05/2023
-	 */ 
-//	public PassWordAuthentificationProtocol getProtocol() {
-//	        return this.Pap;
-//	}
 
 }
