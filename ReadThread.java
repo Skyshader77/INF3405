ad59534adf3fdbd2abfee315f5ac29823be8db0b
Alexandre Nguyen <71104409+Skyshader77@users.noreply.github.com>
2023-05-21 10:02:18 -0400

    Update ReadThread.java
    Cleanup the code

--- ReadThread.java
+++ ReadThread.java
@@ -1,17 +1,20 @@
 /**
  * @summary The purpose of this class is to define a class which manages an the inputs from the server
  * @author Alexandre Nguyen & Louis-Phlippe
- * @version 1.0 Last modified on 20/05/2023
+ * @version 2.0 Last modified on 21/05/2023
  */ 
 
 import java.io.*;
 import java.net.*;
 
 public class ReadThread extends Thread{
-	private BufferedReader reader;
+	
+	//ATTRIBUTES
+	private BufferedReader input;
     private Socket socket;
     private Client client;
- 
+  //METHODS
+    
     public ReadThread(Socket socket, Client client) {
         this.socket = socket;
         this.client = client;
@@ -19,9 +22,9 @@ public class ReadThread extends Thread{
     public void run() {
 	        while (true) {
 	        	try {
-	        		reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    	    	String response=reader.readLine();
-	    	    	System.out.println("\n" + response);
+	        		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    	    	String serverResponse=input.readLine();
+	    	    	System.out.println("\n" + serverResponse);
 	            } catch (IOException ex) {
 	                System.out.println("Error reading from server: " + ex.getMessage());
 	                ex.printStackTrace();
