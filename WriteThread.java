28c3c3021d00e25e13373dc04a6d2b7ae5b89339
Alexandre Nguyen <71104409+Skyshader77@users.noreply.github.com>
2023-05-21 09:59:28 -0400

    Update WriteThread.java
    Cleaned up the code and changed the input for disconnecting.

--- WriteThread.java
+++ WriteThread.java
@@ -1,7 +1,8 @@
+
 /**
- * @summary The purpose of this class is to define a class which manages an user's outputs
+ * @summary The purpose of this class is to define a class which manages an user's outputs to the console
  * @author Alexandre Nguyen & Louis-Phlippe
- * @version 1.0 Last modified on 20/05/2023
+ * @version 2.0 Last modified on 20/05/2023
  */ 
 
 
@@ -11,47 +12,28 @@ import java.net.*;
 public class WriteThread extends Thread {
     private Socket socket;
     private Client client;
-    private PrintWriter writer;
+    private PrintWriter output;
     private BufferedReader inputReader;
-    //private static final int AUTHENTICATED= 3;
     public WriteThread(Socket socket, Client client) {
         this.socket = socket;
         this.client = client;
         try {
-            writer = new PrintWriter(socket.getOutputStream(), true);
+            output = new PrintWriter(socket.getOutputStream(), true);
             inputReader = new BufferedReader(new InputStreamReader(System.in));
-          //consoleReader= new BufferedReader(new InputStreamReader(System.in));
         } catch (IOException ex) {
-            System.out.println("Error getting output stream: " + ex.getMessage());
+            System.out.println("Error getting output or input stream: " + ex.getMessage());
             ex.printStackTrace();
         }
     }
  
     public void run() {
     	try {
-//			out = new PrintWriter(socket.getOutputStream(), true);
-//			PassWordAuthentificationProtocol Pap=new PassWordAuthentificationProtocol();
-//			BufferedReader bufferedIn= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//			String inputLine, outputLine;
-//			 // Initiate conversation with client
-//		    outputLine = Pap.processInput(null);
-//		    out.println(outputLine);
-//		    while ((inputLine = bufferedIn.readLine()) != null) {
-//		        outputLine = Pap.processInput(inputLine);
-//		        out.println(outputLine);
-//		        if (outputLine.equalsIgnoreCase("n"))
-//		            break;
-//		    }
-//	        //String clientLeft = clientNumber + " has been swatted and was forced to disconnect.";
-//	        //server.communicateBetweenClients(clientLeft, this);
-//		} catch (IOException e) {
-//			System.out.println("Error handling client# " + clientNumber + ": " + e);
-        String text;
-        do {
-            text = inputReader.readLine();
-            writer.println(text);
-        } while (!text.equals("bye"));
-        socket.close();
+    		String inputtext;
+	        do {
+	        	inputtext = inputReader.readLine();
+	        	output.println(inputtext);
+	        } while (!inputtext.equals("disconnect"));
+	        output.close();
         } catch (IOException ex) {
             System.out.println("Error writing to server: " + ex.getMessage());
         }
