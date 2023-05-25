2e0381d4252dbea086ac5163890a63932f9c49b1
Alexandre Nguyen <71104409+Skyshader77@users.noreply.github.com>
2023-05-25 16:27:11 -0400

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,24 +1,22 @@
 /**
+ /**
  * @summary The purpose of this class is to define a ClientHandler class
  * @author Alexandre Nguyen & Louis-Phlippe
- * @version 2.0 Last modified on 20/05/2023
+ * @version 3.0 Last modified on 25/05/2023
  */ 
 
 
-import java.io. PrintWriter;
-import java.io.DataInputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
-import java.util.Set;
-import java.net.ServerSocket; 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class ClientHandler extends Thread{
 	private Socket socket;
 	private Server server;
 	private int clientNumber;
-	private PrintWriter out;
+	private DataOutputStream out;
 	private String username;
 	private static final int AUTHENTICATED= 3;
 	public ClientHandler(Socket socket, int clientNumber, Server server) {
@@ -27,24 +25,25 @@ public class ClientHandler extends Thread{
 		}
 	public void run() {   
 		try {
-			out = new PrintWriter(socket.getOutputStream(), true);
+			out = new DataOutputStream(socket.getOutputStream());
 			PassWordAuthentificationProtocol Pap=new PassWordAuthentificationProtocol();
-			BufferedReader bufferedIn= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			DataInputStream bufferedIn= new DataInputStream(socket.getInputStream());
 			String inputLine, outputLine;
 			 // Initiate conversation with client
 		    outputLine = Pap.processInput(null);
-		    out.println(outputLine);
+		    out.writeUTF(outputLine);
 		    //while the user has not been authenticated
 		    while (Pap.getState()<AUTHENTICATED) {
-		    	inputLine = bufferedIn.readLine();
-		        outputLine = Pap.processInput(inputLine);
-		        out.println(outputLine);
+		    	inputLine = bufferedIn.readUTF();
+		        outputLine = Pap.processInput(inputLine+"\n");
+		        out.writeUTF(outputLine);
 		    }
 		    username=Pap.getUser();
+		    printLastMessages(out);
 		    //while the user has not been disconnected
-		    while ((inputLine = bufferedIn.readLine()) != null) {
-		    	String formattedOutput=username+inputLine;
-		        out.println(formattedOutput);
+		    while ((inputLine = bufferedIn.readUTF()) != null) {
+		    	String formattedOutput=formatClientInput(inputLine);
+		        out.writeUTF(formattedOutput);
 		        server.communicateBetweenClients(formattedOutput, this);
 		        if (inputLine.equalsIgnoreCase("disconnect"))
 		            break;
@@ -71,8 +70,83 @@ public class ClientHandler extends Thread{
 	 * @author Alexandre Nguyen & Louis-Phlippe
 	 * @version 1.0 Last modified on 19/05/2023
 	 */ 
-	public PrintWriter getWriter() {
+	public DataOutputStream getWriter() {
 	        return this.out;
 	}
 
+	/**
+     * @summary The purpose of this function is to read the username/password combinations from a TXT file
+     * @return a Boolean saying if the operation succeeded or not
+     * @author Alexandre Nguyen & Louis-Antoine
+     * @version 2.0 Last modified on 15/05/2023
+     * 
+     */ 
+    public String formatClientInput(String theInput) {
+    	LocalDateTime now = LocalDateTime.now();
+
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd@HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+
+    	return "[" + username + " - " + this.server.getServerAddress() + ":" + this.server.getPort() + " - " + formattedDateTime + "]: " + theInput;
+    }
+
+    /**
+     * @summary The purpose of this function is to read the username/password combinations from a TXT file
+     * @return a Boolean saying if the operation succeeded or not
+     * @author Alexandre Nguyen & Louis-Antoine
+     * @version 2.0 Last modified on 15/05/2023
+     * 
+     */ 
+    public void saveClientInputTXT(String theInput) throws IOException{
+		BufferedWriter writer = new BufferedWriter(new FileWriter("clientChat.txt", true));
+
+		writer.write(theInput);
+		writer.newLine();
+		writer.close();
+	}
+
+    /**
+     * @summary The purpose of this function is to read the last 15 messages in the server and print them to the user.
+     * @author Alexandre Nguyen & Louis-Antoine
+     * @version 3.0 Last modified on 25/05/2023
+     * 
+*/ 
+    public void printLastMessages(DataOutputStream out) {
+        try {
+        	StringBuilder printToConsole=new StringBuilder();;
+        	String newLine = System.getProperty("line.separator");
+        	
+        	//Reads from file
+        	File f = new File("NSA_Spy_File.txt");
+        	if (!f.exists()) {
+        		BufferedWriter fbiLeak = new BufferedWriter(new FileWriter("NSA_Spy_File.txt"));
+        	}
+        	BufferedReader reader = new BufferedReader(new FileReader("NSA_Spy_File.txt"));
+            Deque<String> messages = new ArrayDeque<String>();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                messages.add(line);
+            }
+           int sizeofDeque=messages.size();
+         //Removes excess messages
+           while (sizeofDeque > 15){
+        	   		messages.removeFirst();
+        	   		sizeofDeque=messages.size();
+           }
+         //Prints to the user
+           Iterator<String> message = messages.iterator();
+           out.writeUTF("Your last available message(s) is/are :"+newLine);
+           while (message.hasNext()) {
+        	   String theOutput=message.next();
+        	   printToConsole.append(theOutput+newLine);
+            }
+           out.writeUTF(printToConsole.toString());
+    	   //out.wr(); 
+    	   out.flush();
+           reader.close();
+        } catch (IOException e) {
+            System.out.println("Failed to read the chat messages from the file.");
+        }
+    }  
 }
