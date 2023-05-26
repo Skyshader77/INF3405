ef4432895679660f42f8b44f40934d47cd654b46
louis-antoinemm <122337644+louis-antoinemm@users.noreply.github.com>
2023-05-26 12:58:47 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -4,22 +4,25 @@ import java.util.Arrays;
 import java.util.Scanner;  // Import the Scanner class
 /**
  * @summary The purpose of this class is to define an User class
- * @author Alexandre Nguyen & Louis-Phlippe
+ * @author Alexandre Nguyen & Louis-Antoine Martel-Marquis
  * @version 4 Last modified on 25/05/2023
  */ 
 public class Client { private static Socket socket;
-//ATTRIBUTES
+	//ATTRIBUTES
 	private String serverAddress;
 	private int portID;
-//Methods
+	
+
 	public Client(String serverAddress, int portID) {
         this.serverAddress = serverAddress;
         this.portID = portID;
     }
+	
+	//Methods
 	public void execute() {
 		 try {
 				socket = new Socket(serverAddress, portID);
-				System.out.format("Connexion launched to server [%s:%d]", serverAddress, portID);
+				System.out.format("Connexion launched to server [%s:%d]\n", serverAddress, portID);
 				new ReadThread(socket, this).start();
 	            new WriteThread(socket, this).start();
 		    }
@@ -33,25 +36,34 @@ public class Client { private static Socket socket;
 		    }
 	}
 	public static void main(String[] args) throws Exception {
-// Adresse et port du serveur 
+
 			Scanner sc = new Scanner(System.in);  // Create a Scanner object
-			System.out.println("Enter port number");
-		    int portID = sc.nextInt();  // Read user input
-		    if (portID<5000 || portID>5050) {
-		    	throw new IllegalArgumentException("Error: Port ID must be between 5000 and 5050");
-		    }
-		    sc.nextLine();
-		    System.out.println("Enter IP address");
+
+		    System.out.println("Please enter desired IP server address in this format (ex: 127.0.0.1)");
+			
 			String serverAddress = sc.nextLine();  
+			
 			// Uses an Arrays method to see if the IP address is valid
 			boolean ipValid = Arrays
 					  .stream(serverAddress.split("."))
 					  .mapToInt(Integer::parseInt)
 					  .filter(x -> x >= 0 && x <= 255) // remove invalid numbers
 					  .toArray().length == 4; // if the resulting length is 4, the ip is valid
-		    if (ipValid=false) {
+		   
+			if (ipValid=false) {
 		    	throw new IllegalArgumentException("Error:  Invalid IP address");
 		    }
+		   
+			//sc.nextLine();
+			
+			System.out.println("Enter port number");
+		   
+			int portID = sc.nextInt();  // Read user input
+		   
+			if (portID<5000 || portID>5050) {
+		    	throw new IllegalArgumentException("Error: Port ID must be between 5000 and 5050");
+		    }
+		    
 		    Client client = new Client(serverAddress, portID);
 	        client.execute();
 
