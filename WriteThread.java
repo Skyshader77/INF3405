c628dcad0dbfeba118c4c53e6ea598e2696afb27
louis-antoinemm <122337644+louis-antoinemm@users.noreply.github.com>
2023-05-26 14:17:31 -0400

    Clean up

--- WriteThread.java
+++ WriteThread.java
@@ -1,10 +1,9 @@
-
 /**
  * @summary The purpose of this class is to define a class which manages an user's outputs to the console
- * @author Alexandre Nguyen & Louis-Phlippe
- * @version 4.0 Last modified on 25/05/2023
+ * @author Alexandre Nguyen & Louis-Antoine Martel-Marquis
+ * @version 5 Last modified on 26/05/2023
+ * 
  */ 
-
 
 import java.io.*;
 import java.net.*;
@@ -16,14 +15,14 @@ public class WriteThread extends Thread {
     private Client client;
     private Scanner sc;
     private DataOutputStream output;
-  //METHODS
+    
+    //METHODS
     public WriteThread(Socket socket, Client client) {
         this.socket = socket;
         this.client = client;
         try {
             output = new DataOutputStream(socket.getOutputStream());
             sc = new Scanner(System.in);
-            //inputReader = new DataInputStream((System.in));
         } catch (IOException ex) {
             System.out.println("Error getting output or input stream: " + ex.getMessage());
             ex.printStackTrace();
@@ -32,12 +31,15 @@ public class WriteThread extends Thread {
  
     public void run() {
     	try {
+    		
     		String inputtext;
+    		
     		//while the user has not disconnected
 	        do {
 	        	inputtext = sc.nextLine();
 	        	output.writeUTF(inputtext);
 	        } while (!inputtext.equals("disconnect"));
+	        
 	        try {
 	        	output.close();
 	            socket.close();
@@ -45,6 +47,7 @@ public class WriteThread extends Thread {
 	 
 	            System.out.println("Error writing to server: " + ex.getMessage());
 	        }
+	        
         } catch (IOException ex) {
             System.out.println("Error writing to server: " + ex.getMessage());
             ex.printStackTrace();
